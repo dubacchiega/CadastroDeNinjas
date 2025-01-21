c4fb5392f96b04e69273a4be3d34a228bf34360b
Eduardo Bacchiega <dubacchiega@gmail.com>
2025-01-21 10:11:28 -0300

    Cria uma entidade no banco de dados a partirt do NinjaModel

--- src/main/java/br/com/bacchiega/CadastroDeNinjas/NinjaController.java
+++ src/main/java/br/com/bacchiega/CadastroDeNinjas/NinjaController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping()
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota";
     }
-
 
 }
