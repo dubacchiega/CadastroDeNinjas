86d72d9db0f4ad85b76103a5bc2b5336570d8262
Eduardo Bacchiega <dubacchiega@gmail.com>
2025-01-28 10:44:54 -0300

    feat: Cria as rotas do controller de missoes com mock

--- src/main/java/br/com/bacchiega/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/br/com/bacchiega/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package br.com.bacchiega.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missoes listadas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
 }
